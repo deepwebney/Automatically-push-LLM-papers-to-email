9931b2d5c832c8469df5a303a6d47ff9c457becf
deepwebney <49145634+deepwebney@users.noreply.github.com>
2024-03-17 11:37:21 +0800

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -3,10 +3,10 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
-def send_email(receiver_emails=['892904582@qq.com'], title="test", text="", html=""):
-    sender_email = "1157118923@qq.com"  # 发件人邮箱
-    # receiver_email = "sunmengxin98@163.com"  # 收件人邮箱
-    password = "adpyyfdfthdbjgfc"  # QQ邮箱授权码
+def send_email(receiver_emails=[to_email_list], title="test", text="", html=""):
+    sender_email = "your_emial"  # 发件人邮箱
+    # receiver_email = ""  # 收件人邮箱
+    password = ""
 
     message = MIMEMultipart("alternative")
     message["Subject"] = title
@@ -18,7 +18,6 @@ def send_email(receiver_emails=['892904582@qq.com'], title="test", text="", html
     message.attach(part1)
     message.attach(part2)
 
-    # 注意QQ邮箱的SMTP服务器地址是smtp.qq.com，端口号为465
     with smtplib.SMTP_SSL('smtp.qq.com', 465) as server:
         server.login(sender_email, password)
         for r in receiver_emails:
@@ -30,5 +29,5 @@ def send_email(receiver_emails=['892904582@qq.com'], title="test", text="", html
     
     
 if __name__ == '__main__':
-    send_email(['zhangxiao_202003@163.com'], '宣朴副业第一弹')
+    send_email([''], '')
     
